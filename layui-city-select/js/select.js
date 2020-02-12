70912ae7915d2197a66445edfdeda4179322cdc4
jcleng <j.c.leng@foxmail.com>
2020-02-12 09:39:17 +0800

    带初始值进行初始化

--- layui-city-select/js/select.js
+++ layui-city-select/js/select.js
@@ -11,13 +11,17 @@ layui.use(['form', 'layedit', 'laydate', 'upload', "jquery", "pca"], function ()
     var $ = layui.$
         , form = layui.form
         , pca = layui.pca;
-    //带初始值进行初始化
-    pca.init('select[name=P1]', 'select[name=C1]', 'select[name=A1]', '浙江', '杭州', '滨江区');
-
-    //不带初始值
+    // 带初始值进行初始化
+    // pca.init('select[name=P1]', 'select[name=C1]', 'select[name=A1]', '浙江', '杭州', '滨江区');
+    // 带初始值进行初始化
+    var defaultp  = $('select[name=P1]').data('default');
+    var defaultc  = $('select[name=C1]').data('default');
+    var defaulta  = $('select[name=A1]').data('default');
+    pca.init('select[name=P1]', 'select[name=C1]', 'select[name=A1]', defaultp, defaultc, defaulta);
+    // 不带初始值
     // pca.init('select[name=P1]', 'select[name=C1]', 'select[name=A1]');
 
-    //输入提示
+    // 输入提示
     $("#address").bind('input propertychange', function () {
         var address = document.getElementById("address").value;
         if (address == "") {
